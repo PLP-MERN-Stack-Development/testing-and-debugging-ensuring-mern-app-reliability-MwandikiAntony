bce52c9c6e40ac03e11fdd1551aa769796ee3a39
MwandikiAntony <antonymwandiki26@gmail.com>
2025-11-09 16:08:47 +0300

    added test routes

--- backend/tests/bugRoutes.test.js
+++ backend/tests/bugRoutes.test.js
@@ -2,13 +2,3 @@
 const request = require('supertest');
 const app = require('../server');
 
-describe('Bug API Routes', () => {
-  it('should create a bug', async () => {
-    const response = await request(app).post('/api/bugs').send({
-      title: 'Bug 1',
-      description: 'Sample Bug',
-      status: 'open'
-    });
-    expect(response.status).toBe(201);
-  });
-});
