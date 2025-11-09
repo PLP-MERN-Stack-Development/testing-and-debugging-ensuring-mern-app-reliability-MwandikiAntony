8df39c9f3bf9b2ff3c2c0f2089cbef312fa668e6
MwandikiAntony <antonymwandiki26@gmail.com>
2025-11-09 16:19:27 +0300

    deleted some functions to the bug list

--- frontend/src/components/BugList.js
+++ frontend/src/components/BugList.js
@@ -2,33 +2,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-function BugList() {
-  const [bugs, setBugs] = useState([]);
 
-  useEffect(() => {
-    const fetchBugs = async () => {
-      const response = await axios.get('http://localhost:5000/api/bugs');
-      setBugs(response.data);
-    };
-    fetchBugs();
-  }, []);
-
-  return (
-    <div className="bug-list">
-      <h2>Reported Bugs</h2>
-      <ul>
-        {bugs.map((bug) => (
-          <li key={bug._id}>
-            <h3>{bug.title}</h3>
-            <p>{bug.description}</p>
-            <p>Status: {bug.status}</p>
-            <button>Update Status</button>
-            <button>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
 
 export default BugList;
