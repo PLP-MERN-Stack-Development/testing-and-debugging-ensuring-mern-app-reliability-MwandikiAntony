f9ce0cf7b2ef939c7b889c1c1d40be87a5cf430c
MwandikiAntony <antonymwandiki26@gmail.com>
2025-11-09 16:11:35 +0300

    updated server

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,4 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(cors());
-
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
-
-app.use("/api/bugs", require("./routes/bugs"));
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
