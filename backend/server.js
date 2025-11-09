1f7c7fdf0629bd8701e295ab4d1ae2695e700b5a
MwandikiAntony <antonymwandiki26@gmail.com>
2025-11-09 17:26:51 +0300

    added .env file

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || '*'
+}));
+
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
