67b51852cb5b58c5ad4538b5c66d733962fb2be1
MwandikiAntony <antonymwandiki26@gmail.com>
2025-11-09 16:06:25 +0300

    added vug model in backend

--- backend/models/Bug.js
+++ backend/models/Bug.js
@@ -1,4 +1,10 @@
 
 const mongoose = require("mongoose");
 
+const BugSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  status: { type: String, enum: ['open', 'in-progress', 'resolved'], default: 'open' },
+}, { timestamps: true });
 
+module.exports = mongoose.model("Bug", BugSchema);
