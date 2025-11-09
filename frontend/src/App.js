21af1b0457848ca8a4169fbfbd83639136da9058
MwandikiAntony <antonymwandiki26@gmail.com>
2025-11-09 16:22:19 +0300

    updated app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BugForm from './components/BugForm';
 import BugList from './components/BugList';
 
-
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1>Bug Tracker</h1>
+        <Routes>
+          <Route path="/bugs" component={BugList} />
+          <Route path="/new" component={BugForm} />
+          <Route path="/" exact component={BugList} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
