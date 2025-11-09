cce51baa3912c004ca9c261886a26b4479aeb4d4
MwandikiAntony <antonymwandiki26@gmail.com>
2025-11-09 16:18:32 +0300

    added some rendering to the test form

--- frontend/src/components/BugForm.test.js
+++ frontend/src/components/BugForm.test.js
@@ -2,7 +2,11 @@
 import { render, fireEvent, screen } from '@testing-library/react';
 import BugForm from './BugForm';
 
-
+test('it should submit the form', async () => {
+  render(<BugForm />);
+  fireEvent.change(screen.getByLabelText(/title/i), { target: { value: 'Test Bug' } });
+  fireEvent.change(screen.getByLabelText(/description/i), { target: { value: 'Bug description' } });
+  fireEvent.click(screen.getByText(/report bug/i));
 
   expect(await screen.findByText('Bug reported successfully!')).toBeInTheDocument();
 });
